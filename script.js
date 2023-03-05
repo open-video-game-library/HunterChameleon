151c87a4e8d7e06b6001babf25abe79b924759c1
T-Kawashima-fms <ttttt.fms5@gmail.com>
2023-03-05 20:59:50 +0900

    deploy

---
+++ script.js
@@ -0,0 +1,47 @@
+var datas = [];
+
+function addJsonData(jsonObj) {
+    jsonObj.id = datas.length + 1;
+    datas.push(jsonObj);
+}
+
+function downloadJsonData() {
+    let loghead = "id,score,hitNum,hitRate\n";
+    let logdata = "";
+    datas.map(function (d) {
+        logdata += d.id + "," + d.score + "," + d.hit_num + "," + d.hit_rate + "\n";
+    });
+
+    const filename = getNow() + ".csv";
+    const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+    const blob = new Blob([bom, loghead + logdata], { type: "text/csv" });
+
+    if (window.navigator.msSaveBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    } else {
+        const url = (window.URL || window.webkitURL).createObjectURL(blob);
+        const download = document.createElement("a");
+        download.href = url;
+        download.download = filename;
+        download.click();
+        (window.URL || window.webkitURL).revokeObjectURL(url);
+    }
+}
+
+// 時刻を取得
+function getNow() {
+    var date = new Date();
+    var yyyy = date.getFullYear();
+    var mm = toDoubleDigits(date.getMonth() + 1);
+    var dd = toDoubleDigits(date.getDate());
+    var hh = toDoubleDigits(date.getHours());
+    var mi = toDoubleDigits(date.getMinutes());
+    return yyyy + mm + dd + '_' + hh + mi;
+}
+function toDoubleDigits(num) {
+    num += "";
+    if (num.length === 1) {
+        num = "0" + num;
+    }
+    return num;   
+}
