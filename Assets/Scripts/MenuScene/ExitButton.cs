aed91969a1bc7bd49ce70859a5638b3f7b9d2dde
T-Kawashima-fms <ttttt.fms5@gmail.com>
2023-03-11 15:30:24 +0900

    style: inactive quit-btn when webgl

--- Assets/Scripts/MenuScene/ExitButton.cs
+++ Assets/Scripts/MenuScene/ExitButton.cs
@@ -5,12 +5,19 @@ using System.Runtime.InteropServices;
 
 public class ExitButton : MonoBehaviour
 {
+    private void Start ()
+    {
+        #if UNITY_WEBGL
+            this.gameObject.SetActive(false);
+        #endif
+    }
+
     public void OnClicked ()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif !UNITY_WEBGL
+            Application.Quit();
+        #endif
     }
 }
